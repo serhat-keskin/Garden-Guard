595f9298d5cc760b0bcf0d3dbcac21ecee5a1968
serhat-keskin <serhatkeskin710yedek@gmail.com>
2025-01-19 10:10:00 +0300

    Add Key collectible logic

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            col.GetComponent<Inventory>().isHaveKey = true;
+            Destroy(gameObject);
+        }
+    }
+}
